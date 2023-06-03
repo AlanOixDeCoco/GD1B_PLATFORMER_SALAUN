9a3b16b5f7213d71e04b6f36916dee7b9bc2cee1
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2023-06-03 19:09:33 +0200

    c'est trop

--- Assets/Scripts/Components/State.js
+++
@@ -1,9 +0,0 @@
-// Not an actual Interface (thanks js...)
-export default class IState {
-    constructor(stateType){
-        this._type = stateType;
-    }
-    Tick(){}
-    OnEnterState(){}
-    OnExitState(){}
-}
